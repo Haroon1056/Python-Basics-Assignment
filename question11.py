b8549dbd842763e22da1ed929b3657682989d1e8
Haroon Rasheed <haroonrasheed1056@gmail.com>
2024-11-10 00:29:08 +0500

    Delete question11.py

--- question11.py
+++
@@ -1,8 +0,0 @@
-# 11. Check if a given number is a multiple of both 3 and 5.
-
-num = int(input("Enter the Number: "))
-
-if num % 3 == 0 and num % 5 == 0:
-    print(num, "mutiple both 3 and 5")
-else:
-    print(num, "not mutiple both 3 and 5")
