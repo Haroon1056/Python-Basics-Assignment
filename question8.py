58e320d2d7b0940c032111a6de624cffa71b02c1
Haroon Rasheed <haroonrasheed1056@gmail.com>
2024-11-10 00:33:16 +0500

    Delete question8.py

--- question8.py
+++
@@ -1,8 +0,0 @@
-# 8. Create a program that checks if a given string is a palindrome.
-
-str = "PakistanatsikaP"
-
-if str == str[::-1]:
-    print(str, "is palindrome")
-else:
-    print(str, "isn't palindrome")
