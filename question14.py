1f244d00228860a24d2f5dedb5c98adfdb4c8888
Haroon Rasheed <haroonrasheed1056@gmail.com>
2024-11-10 00:30:00 +0500

    Delete unnecessary file

--- question14.py
+++
@@ -1,8 +0,0 @@
-# 14. Check if a year input by the user is a century year.
-
-year = int(input("Enter the Year Number: "))
-
-if year % 100 == 0:
-    print(f"{year} is a Century Year")
-else:
-    print(f"{year} isn't a Century Year")
